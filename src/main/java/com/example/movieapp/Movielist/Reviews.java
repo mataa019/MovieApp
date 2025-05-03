ffc38c3892c94d41c83b1b197aa7b08493d83150
mataa019 <johnnymataa019@gmail.com>
2025-05-04 00:38:39 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/Movielist/Reviews.java
+++ src/main/java/com/example/movieapp/Movielist/Reviews.java
@@ -1,8 +1,15 @@
 package com.example.movieapp.Movielist;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collation = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reviews {
+    private
 
 }
