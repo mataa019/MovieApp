4eb257f6a562a27cb27588c2ffeb59a4823cd6c5
mataa019 <johnnymataa019@gmail.com>
2025-05-03 14:47:56 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/Movielist/MovieModel.java
+++ src/main/java/com/example/movieapp/Movielist/MovieModel.java
@@ -10,4 +10,28 @@ public class MovieModel {
         Director = director;
         Year = year;
     }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public Integer getYear() {
+        return Year;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    public void setYear(Integer year) {
+        Year = year;
+    }
 }
