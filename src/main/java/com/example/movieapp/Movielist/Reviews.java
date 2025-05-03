361236e1d8b947199b53c1201583309ea73d1045
mataa019 <johnnymataa019@gmail.com>
2025-05-04 00:39:17 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/Movielist/Reviews.java
+++ src/main/java/com/example/movieapp/Movielist/Reviews.java
@@ -3,6 +3,7 @@ package com.example.movieapp.Movielist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collation = "reviews")
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Reviews {
-    private
+    private ObjectId id;
+    private String reviewBody;
 
 }
