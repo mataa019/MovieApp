e4f5c61d5f390a296e9aa1297d8f00cc785c915a
mataa019 <johnnymataa019@gmail.com>
2025-05-04 00:53:30 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/MovieController.java
+++ src/main/java/com/example/movieapp/MovieController.java
@@ -1,19 +1,13 @@
 package com.example.movieapp;
 
 import com.example.movieapp.Movielist.MovieModel;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@RequestMapping("/api/v1/movies")
 public class MovieController {
-    MovieModel  model = new MovieModel("John Walker", "Mike", 2015);
+   @GetMapping
 
-    @GetMapping("/model/{getallMovies}")
-    public void GetListofMovies(@PathVariable String getallMovies)
-    {
-    }
+
 
 }
