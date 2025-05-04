7b142473377cc7d229a0bdd9b2692089aec22600
mataa019 <johnnymataa019@gmail.com>
2025-05-04 23:25:07 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/MovieController.java
+++ src/main/java/com/example/movieapp/MovieController.java
@@ -1,16 +1,22 @@
 package com.example.movieapp;
 
 import com.example.movieapp.Movielist.MovieModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+   @Autowired
+   private  MovieService movieService;
    @GetMapping
-   public ResponseEntity<String> getallMovies(){
-      return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+   public ResponseEntity<List<MovieModel>> getallMovies(){
+      return new ResponseEntity<List<MovieModel>>(movieService.allMovies(),HttpStatus.OK);
    }
 
 
