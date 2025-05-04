58e5b42ff73d534201a651aa7045c474e1c7c8d2
mataa019 <johnnymataa019@gmail.com>
2025-05-04 23:14:24 +0200

    implementing movie App

--- src/main/java/com/example/movieapp/MovieRepository.java
+++ src/main/java/com/example/movieapp/MovieRepository.java
@@ -1,6 +1,10 @@
 package com.example.movieapp;
 
+import com.example.movieapp.Movielist.MovieModel;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MovieRepository extends MongoRepository<Movie. Object> {
+@Repository
+public interface MovieRepository extends MongoRepository<MovieModel, ObjectId> {
 }
